e2b68dedf6a81bc34176b8a820f645d3b2feef53
Kevin Jones <bioncl7@gmail.com>
2019-10-14 21:36:45 -0500

    Separated Rock into its own file

--- src/Main.java
+++ src/Main.java
@@ -74,13 +74,13 @@ public class Main extends Application {
 		scoreText.setFont(new Font(30));
 		scoreText.setFill(Color.WHITE);
 		Group mainGroup = new Group(scene, scoreText);
-		Scene mainScene = new Scene(mainGroup, 1280, 720);
+		Scene mainScene = new Scene(mainGroup, 1250, 900);
 		scene.widthProperty().bind(mainScene.widthProperty());
 		scene.heightProperty().bind(mainScene.heightProperty());
 
 		stage.setTitle("Space Brawl Prototype");
 		stage.setScene(mainScene);
-		stage.setMaximized(true);
+		//stage.setMaximized(true);
 		scene.setCursor(Cursor.NONE);
 		stage.show();
 
@@ -179,69 +179,5 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		Application.launch(args);
-	}
-}
-
-class Rock extends MeshView {
-	private double radius;
-	private Random random;
-
-	public Rock(double radius) {
-		super();
-		this.radius = radius;
-		random = new Random();
-		setMesh(buildMesh((float)radius));
-	}
-
-	public double getRadius() {
-		return radius;
-	}
-
-	private TriangleMesh buildMesh(float radius) {
-		TriangleMesh mesh = new TriangleMesh();
-		mesh.getTexCoords().addAll(0, 0);
-		mesh.getPoints().addAll(0, -genDistance(radius), 0);
-		mesh.getPoints().addAll(0, genDistance(radius), 0);
-		for (int i = 0; i < 10; i++) {
-			double yaw = 2 * Math.PI * i / 10;
-			for (int j = 1; j < 10; j++) {
-				double pitch = Math.PI * (-0.5 + (double)j / 10);
-
-				float distance = genDistance(radius);
-				float x = (float)Math.sin(yaw);
-				float z = (float)Math.cos(yaw);
-				float s = distance * (float)Math.cos(pitch);
-				x *= s;
-				z *= s;
-				float y = distance * (float)Math.sin(pitch);
-
-				mesh.getPoints().addAll(x, y, z);
-			}
-		}
-		for (int i = 0; i < 10; i++) {
-			mesh.getFaces().addAll(2 + i * 9, 0);
-			mesh.getFaces().addAll(0, 0);
-			mesh.getFaces().addAll(2 + (i + 1) % 10 * 9, 0);
-
-			for (int j = 0; j < 10 - 2; j++) {
-				mesh.getFaces().addAll(2 + i * 9 + j, 0);
-				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j, 0);
-				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j + 1, 0);
-
-				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j + 1, 0);
-				mesh.getFaces().addAll(2 + i * 9 + j + 1, 0);
-				mesh.getFaces().addAll(2 + i * 9 + j, 0);
-			}
-
-			mesh.getFaces().addAll(1, 0);
-			mesh.getFaces().addAll(2 + i * 9 + 8, 0);
-			mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + 8, 0);
-		}
-
-		return mesh;
-	}
-
-	private float genDistance(float radius) {
-		return radius - random.nextFloat() * radius * 0.2f;
 	}
 }
---
+++ src/Rock.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import javafx.scene.shape.*;
+
+class Rock extends MeshView {
+	private double radius;
+	private Random random;
+
+	public Rock(double radius) {
+		super();
+		this.radius = radius;
+		random = new Random();
+		setMesh(buildMesh((float)radius));
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	private TriangleMesh buildMesh(float radius) {
+		TriangleMesh mesh = new TriangleMesh();
+		mesh.getTexCoords().addAll(0, 0);
+		mesh.getPoints().addAll(0, -genDistance(radius), 0);
+		mesh.getPoints().addAll(0, genDistance(radius), 0);
+		for (int i = 0; i < 10; i++) {
+			double yaw = 2 * Math.PI * i / 10;
+			for (int j = 1; j < 10; j++) {
+				double pitch = Math.PI * (-0.5 + (double)j / 10);
+
+				float distance = genDistance(radius);
+				float x = (float)Math.sin(yaw);
+				float z = (float)Math.cos(yaw);
+				float s = distance * (float)Math.cos(pitch);
+				x *= s;
+				z *= s;
+				float y = distance * (float)Math.sin(pitch);
+
+				mesh.getPoints().addAll(x, y, z);
+			}
+		}
+		for (int i = 0; i < 10; i++) {
+			mesh.getFaces().addAll(2 + i * 9, 0);
+			mesh.getFaces().addAll(0, 0);
+			mesh.getFaces().addAll(2 + (i + 1) % 10 * 9, 0);
+
+			for (int j = 0; j < 10 - 2; j++) {
+				mesh.getFaces().addAll(2 + i * 9 + j, 0);
+				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j, 0);
+				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j + 1, 0);
+
+				mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + j + 1, 0);
+				mesh.getFaces().addAll(2 + i * 9 + j + 1, 0);
+				mesh.getFaces().addAll(2 + i * 9 + j, 0);
+			}
+
+			mesh.getFaces().addAll(1, 0);
+			mesh.getFaces().addAll(2 + i * 9 + 8, 0);
+			mesh.getFaces().addAll(2 + (i + 1) % 10 * 9 + 8, 0);
+		}
+
+		return mesh;
+	}
+
+	private float genDistance(float radius) {
+		return radius - random.nextFloat() * radius * 0.2f;
+	}
+}
