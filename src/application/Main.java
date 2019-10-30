48450f11ca1d709e6452b49930e30d620a12c596
noor <noor.ask@hotmail.com>
2019-10-30 13:43:04 -0500

    added exit

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,6 +8,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
