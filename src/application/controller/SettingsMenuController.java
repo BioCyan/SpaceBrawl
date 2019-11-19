2b2e8d95b6a2384e73c9b850fe478459b2c883fb
Kevin Jones <kjones@nd4c.com>
2019-11-19 10:53:06 -0600

    Tweaking settings view

--- src/application/controller/SettingsMenuController.java
+++ src/application/controller/SettingsMenuController.java
@@ -19,32 +19,7 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class SettingsMenuController {
-    ChangeController controllerChange;
-    @FXML Button rightButton;
-    private Stage stage;
+	public void clickButton(ActionEvent event) {
 
-    public void rightClick(javafx.event.ActionEvent event) throws IOException{
-        String rightValue = "D";
-        controllerChange.setScene(rightValue);
-
-    }
-
-    //Start method is used to switch views
-    public void start(Stage stage) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/Settings.fxml"));
-        Parent root = loader.load();
-        SettingsMenuController controller = loader.getController();
-        controller.initialize(stage);
-        stage.setScene(new Scene(root, 800, 600));
-        stage.setTitle("Settings Page");
-        stage.show();
-    }
-
-    public void clickButton(ActionEvent event) {
-    }
-
-    public void initialize(Stage stage) {
-        this.stage = stage;
-    }
-
+	}
 }
