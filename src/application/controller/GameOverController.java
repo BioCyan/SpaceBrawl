fa3c446981ccae368ce5af2dce4eaaa1e445801a
Kevin Jones <kjones@nd4c.com>
2019-11-26 10:11:22 -0600

    Prepared GameOver view for showing shot count and final score

--- src/application/controller/GameOverController.java
+++ src/application/controller/GameOverController.java
@@ -3,9 +3,13 @@ package application.controller;
 import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import java.io.IOException;
 
 public class GameOverController {
+	@FXML private Label shotsLabel;
+	@FXML private Label scoreLabel;
+
 	@FXML
 	public void menu(ActionEvent e) {
 		Main.switchScene(Main.SceneType.Main);
