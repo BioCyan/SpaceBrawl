0181e913ac097a5edbeff6aa40a83c1e859db61a
Kevin Jones <kjones@nd4c.com>
2019-11-25 23:43:11 -0600

    Made the star glow

--- src/application/model/Star.java
+++ src/application/model/Star.java
@@ -1,11 +1,18 @@
 package application.model;
 
 import javafx.geometry.Point3D;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 
 public class Star extends Sphere {
 	public Star() {
 		super(1);
+		PhongMaterial material = new PhongMaterial(Color.rgb(255, 192, 96));
+		Image white = new Image(Star.class.getResourceAsStream("/white.png"));
+		material.setSelfIlluminationMap(white);
+		setMaterial(material);
 	}
 
 	public boolean checkCollision(Player player) {
